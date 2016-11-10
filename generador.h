d0118dd70c75e905633217f0bd8f698cdf8d404e
Natalia Cardozo <talycardozo@gmail.com>
2016-11-10 19:19:17 -0300

    Validacion de entrada de parametros

--- generador.h
+++ generador.h
@@ -11,23 +11,51 @@ clock_t start;
 
 void cargaparametros(){
 	FILE *ptrcf;
-	char aux;
+	char aux[10];
 	if((ptrcf=fopen("parametros.dat","w"))!=NULL){
 		printf("%s\n", "Ingrese Cantidad de pisos del edificio" );
 		do{
-			scanf("%d",&n);
+			puts("5 - 10")
+			scanf("%s",aux);
+			if (valido(aux)) n=atoi(aux);
+			else printf("\a\nERROR\n");
 		}while (n>10 || n<5);
+
 		printf("%s\n", "Ingrese Cantidad maxima de personas para el elevador" );
 		do{
-			scanf("%d",&c);
+			puts("4 - 20");
+			scanf("%s",aux);
+			if (valido(aux)) c=atoi(aux);
+			else printf("\a\nERROR\n");
 		}while (c>20 || c<4);
+
 		printf("%s\n", "Tiempo en segundos para que un usuario suba o baje del ascensor" );
-		scanf("%d",&tu);
-		printf("%s\n", "Tiempo en segundos desplazamiento del ascensor de un piso a otro" );
-		scanf("%d",&tp);
+		do{
+			puts("1 - 60");
+			scanf("%s",aux);
+			if (valido(aux)) tu=atoi(aux);
+			else printf("\a\nERROR\n");
+		}while(tu<1 || tu>60); //minimo 1 segundo - maximo 1 minuto 
+
+		printf("%s\n", "Tiempo en segundos del desplazamiento del ascensor de un piso a otro" );
+		do{
+			puts("1 - 60");
+			scanf("%s",aux);
+			if (valido(aux)) tp=atoi(aux);
+			else printf("\a\nERROR\n");
+		}while(tp<1 || tp>60); //minimo 1 segundo - maximo 1 minuto
+		
 		fprintf(ptrcf, "%d %d %d %d\n",n,c,tu,tp);
 		fclose(ptrcf);
 	}
+}
+
+int valido(char x[]){
+	int i;
+	for (i=0;i<strlen(x);i++){
+		if(!isdigit(x[i])) return 0;
+	}
+	return 1;
 }
 
 void leerparametros(){
@@ -40,11 +68,12 @@ void leerparametros(){
 
 void generador(){
 	srand(time(NULL));
-	s=rand()%(n*c);
+	s=rand()%1001;
 	printf("%d\n",(int)start);
 	FILE *ptrcf;
 	int i,pdestino,porigen;
 	if((ptrcf=fopen("solicitudes.dat","w"))!=NULL){
+		if (s==0) fprintf(ptrcf, "%d %d %d\n",0,0,0); //no existe llamado al ascensor en ese momento
 		for (i=0;i<s;i++){
 			clock_t tiempo=clock();
 			porigen=rand()%n+1;
