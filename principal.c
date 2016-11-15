0e74f4c41a190d1cf4ec7466e6f4d8a937c80c4a
BelSaldivar <paldbernal@gmail.com>
2016-11-15 12:44:23 -0300

    YA FUNCIONA MI COLA GENTE

--- principal.c
+++ principal.c
@@ -1,18 +1,28 @@
 #include "generador.h"
+#include "estructurador.h"
 #include <unistd.h>
+
 
 int main(int argc, char const *argv[])
 {
 	int ts;
-	cargaparametros();
+	//cargaparametros();
 	leerparametros();
 	scanf("%d",&ts);
 	generadorts(ts);
-	time_t start=time(NULL);
-	while(1==1){
-		sleep(1);
-
-		printf("%d\n", n);
+	struct cola C[n+1];
+	int i;
+	for (i=0;i<n+1;i++){
+		C[i].primero=NULL;
+		C[i].ultimo=NULL;
 	}
+	leerarchivo(C);
+	imprimecola(C);
+	time_t start=time(NULL);
+//	while(1==1){
+//		sleep(1);
+//
+//		printf("%d\n", n);
+//	}
 	return 0;
 }
