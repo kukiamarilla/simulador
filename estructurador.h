da4f5bdfacd3b06808d8676e9711d16d04b41bd2
BelSaldivar <paldbernal@gmail.com>
2016-11-15 11:42:06 -0300

    Cambios en estructurador

--- estructurador.h
+++ estructurador.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "generador.h"
+
 
 FILE *ptrCf;  //ptrCf = apuntador al archivo solicitudes.dat
 
@@ -15,29 +15,28 @@ struct cola{
 	struct elemento * ultimo;
 };
 
-int n=5;
 void leerarchivo(struct cola C[]);  //funcion que lee el archivo solicitudes.dat
 void encolar(struct cola *C, int pdestino, int tiempo);  //funcion que pasa los datos del archivo solicitudes.dat a una cola
 void imprimecola(struct cola c[]);
-
+int n,s,c,tu,tp;
 
 void encolar(struct cola *C, int pdestino, int tiempo){
 	struct elemento * nuevo;
-  
 	nuevo = (struct elemento *) malloc(sizeof(struct elemento));
 	if (nuevo!=NULL){
 		nuevo->sig=NULL;  /* siempre se pone al final */
 		nuevo->pdestino=pdestino;
 		nuevo->tiempo=tiempo;
-	
 	  /* ponerlo al final */
 	
 	  /* si esta vacia la cola */
 		if ( C->primero == NULL ){ /* si primero es NULL -> ultimo tambien */
+			puts("primer nodo");
 	    	C->ultimo=nuevo;
 	    	C->primero=nuevo;
 		}else {
 	    /* caso general */   
+	    	puts("nodo sig");
 	    	C->ultimo->sig=nuevo;    
 			C->ultimo=nuevo;
 		}
@@ -50,9 +49,9 @@ void imprimecola(struct cola c[]){
 		if(c[i].primero==NULL){
 			puts("cola vacia");
 		}else{
-			printf("la cola es del piso %d es:",i);
+			printf("\nla cola del piso %d es:",i);
 			while(c[i].primero!=NULL){
-				printf("%d %d",c[i].primero->pdestino,c[i].primero->tiempo);
+				printf(" %d %d -",c[i].primero->pdestino,c[i].primero->tiempo);
 				c[i].primero=c[i].primero->sig;
 			}
 		}
@@ -61,12 +60,15 @@ void imprimecola(struct cola c[]){
 
 //funcion que lee el archivo solicitudes.dat y pasa los datos del archivo a una cola
 void leerarchivo(struct cola C[]){  
+	leerparametros();
 	int porigen, pdestino, tiempo;
 	FILE *ptrcf;
-	printf("leer archivo: ");
 	if((ptrcf=fopen("solicitudes.dat","r"))!=NULL){
-		fscanf(ptrcf,"%d %d %d ", &porigen, &pdestino, &tiempo);
-		encolar(&(C[porigen]), pdestino, tiempo);
+		while (!feof(ptrcf)){
+			fscanf(ptrcf,"%d%d%d\n", &porigen, &pdestino, &tiempo);
+			printf("\n%d %d %d\n",porigen,pdestino,tiempo);
+			encolar(&(C[porigen]), pdestino, tiempo);
+		}
 		fclose(ptrcf);
 	}	
 	
