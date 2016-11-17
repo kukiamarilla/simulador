545da7931ce478e73a18abbb1e58403f25c33500
Isaac Amarilla <sc.amarilla@gmail.com>
2016-11-16 22:05:29 -0300

    modificaciones en apilador

--- apilador.h
+++ apilador.h
@@ -1,42 +1,25 @@
 
+int subir=0;
 
-int desencolar(struct cola *C){						//DESENCOLA EL VECTOR DE COLAS DE ESTRUCTURADOR.H Y RETORNA UN "TIPO ESTRUCTURA"
-	int ax;
-	struct elemento *temporal;
-    if(C->primero==NULL){
-        ax=0;
-	}else{
-	    ax=C->primero->pdestino;					
-	    temporal=C->primero;
-	    C->primero=(C->primero)->sig;
-	    if(C->primero == NULL){
-	        C->ultimo=NULL;
-	    }
-	    free(temporal);
-    }
+
+int desencolar(struct cola * C){						//DESENCOLA EL VECTOR DE COLAS DE ESTRUCTURADOR.H Y RETORNA UN "TIPO ESTRUCTURA"
 	
-	return ax;
+	struct elemento *temporal;
+	struct elemento aux;
+    if(C->primero!=NULL){
+        temporal=C->primero;
+        C->primero=C->primero->sig;
+        aux=*temporal;
+        free(temporal);
+	    
+    }else{
+    	return NULL;
+    }
+	return aux;
+
 
 }
-int contar(struct nodo **lista,int pis){									//PREGUNTA SI EL PISO AL QUE DESEA IR YA ESTA EN LA LISTA, SI ESTA ES AGREGADA EN DICHO NODO COMO UNA PERSONA MAS QUE DESEA IR
-	struct nodo *actual,*anterior;
-	anterior=NULL;
-	actual=*lista;
-	int a=0; int ha=0; int t;
-	while(actual!=NULL && !a){
-        a=(actual->piso==pis);
-	        if(!a){
-	            anterior=actual;
-	            actual=actual->sig;
-	        } else {
-		        t=actual->paso; 
-				t=t+1;
-				actual->paso=t;
-				ha=1;
-	        }
-	}
-	return ha;
-}
+
 
 void insertar( struct nodo **lista, struct nodo**ultimo,  int dato ){
 	int b;
@@ -55,108 +38,91 @@ void insertar( struct nodo **lista, struct nodo**ultimo,  int dato ){
 		b=contar(&(*lista), dato);
 		if(b==0){
 			nuevo= (struct nodo *)malloc( sizeof( struct nodo ) );
-							if(nuevo!=NULL){
-								nuevo->piso=dato;
-								while(actual!=NULL){
-								            
-								        if(dato<actual->piso){
-								            if(actual==*lista){
-								                *lista=nuevo;
-								            }
-								            nuevo->ant=actual->ant;
-								            nuevo->sig=actual;
-								            if(actual->ant!=NULL){
-								                actual->ant->sig=nuevo;
-								            }
-								            actual->ant=nuevo;
-								            break;
-								        }
-								        else{
-								            if(actual->sig==NULL){
-								                nuevo->ant=actual;
-								                actual->sig=nuevo;
-								                *ultimo=nuevo;
-								                break;
-								            }
-								            actual=actual->sig;
-								        }
-								}
-								
-						}
+			if(nuevo!=NULL){
+				nuevo->piso=dato;
+				while(actual!=NULL){
+				            
+				        if(dato<actual->piso){
+				            if(actual==*lista){
+				                *lista=nuevo;
+				            }
+				            nuevo->ant=actual->ant;
+				            nuevo->sig=actual;
+				            if(actual->ant!=NULL){
+				                actual->ant->sig=nuevo;
+				            }
+				            actual->ant=nuevo;
+				            break;
+				        }
+				        else{
+				            if(actual->sig==NULL){
+				                nuevo->ant=actual;
+				                actual->sig=nuevo;
+				                *ultimo=nuevo;
+				                break;
+				            }
+				            actual=actual->sig;
+				        }
+				}
+				
+		}
 		}
 	}
 }
-void imprimir(struct nodo **lista){
-struct nodo *actual;
-    actual=*lista;
-    while(actual!=NULL){
-        printf("%d",actual->piso );
-        actual=actual->sig;
 
-    }
-
-}
-void eliminar(struct nodo **lista,struct nodo **utlimo,  int dato){															//ELIMINA LOS NODOS QUE YA LLEGARON A DESTINO LIBERANDO LUGAR EN EL ASCENSOR
-    struct nodo *actual;
-	actual=*lista;
-	int a=0;
-	while(actual!=NULL && !a){
-        a=(actual->piso==dato);
-        if(!a){
-            actual=actual->sig;
-        }
-	}
-	if(actual!=NULL){
-        if(actual==*lista){
-        	*lista=actual->sig;
-        	if(actual->sig!=NULL){
-        		actual->sig->ant=NULL;
-        	}	
-		} else if(actual->sig!=NULL){
-			actual->ant->sig=actual->sig;
-			actual->sig->ant=actual->ant;
-		}
-        else{
-        	actual->ant->sig=NULL;
-        	*utlimo=actual->ant;
-        }
-        free(actual);
-	}
-}
-
-
-
-int sumar(struct nodo**lista){						//CALCULA LAS PERSONAS DENTRO DEL ASCENSOR
-	int cantidad,can;
-	int w=1;
-	struct nodo *actual,*anterior;
-	anterior=NULL;
-	actual=*lista;
-	while(actual!=NULL){
-			can=actual->paso; 					//SUMA LA CANTIDAD DE PERSONAS POR PISO DENTRO DEL ASCENSOR
-			cantidad+=can;
-			if(cantidad==c){							//SI LLEGA AL LIMITE TERMINA
-				w=0;
-				break;
-			}
-			anterior=actual;
+void eliminar(int piso){													//ELIMINA LOS NODOS QUE YA LLEGARON A DESTINO LIBERANDO LUGAR EN EL ASCENSOR
+	struct elemento * actual = lista;
+	if(lista != NULL){
+		while(actual->pdestino != piso && actual != NULL){
 			actual=actual->sig;
-
-	}
-return w;	
-}
-void apilador(struct cola C[][], int x, int direc){				//FUNCION DENTRO DEL MAIN ENCARGADO DE LLAMAR A LAS FUNCIONES AUXILIARES
-    int dato;
-    int parar;
-    do{
-        eliminar(&lista, &ultimo, x);
-    	if(direc){
-        	dato=desencolar(&(C[1][x]));
-		} else{
-			dato=desencolar(&(C[0][x]));
 		}
-    	insertar(&lista, &ultimo,dato);
-    	parar=sumar(&lista);
-	}while(parar!=0 && dato!=0);
+		if(actual !=NULL){
+			personas--;
+			if(actual->sig!=NULL){
+				actual->ant->sig=actual->sig;
+			}
+			if(actual->ant!=NULL){
+				actual->sig->ant=actual->ant;
+			}
+			free(actual);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void apilador( int piso, int direc){				//FUNCION DENTRO DEL MAIN ENCARGADO DE LLAMAR A LAS FUNCIONES AUXILIARES
+    int dato;
+    int time;
+    int eliminado=1;
+    struct nodo persona;
+    if(piso != -1){
+    	if(lista!= NULL && eliminado == 1){
+    		if(subir==0){
+    			time=tiempo;
+    		}
+    		subir=1;
+    		if(subir==1 && tiempo-time >= tu){
+	    		eliminado=eliminar(piso);
+	    		if(eliminado==1){
+	    			time=tiempo;
+	    		}
+	    	}
+    	}
+    	if(eliminado==0){
+	    	if(personas < c &&Colas[direc][piso]->primero != NULL && Colas[direc][piso]->primero->tiempo <= tiempo){
+	    		if(subir==0){
+	    			time=tiempo;
+	    		}
+	    		subir=1;
+	    	}
+	    	if(subir==1 && tiempo-time >= tu){
+	    		subir=0;
+	    		persona=desencolar(Colas[direc][piso]);
+	    		insertar(persona);
+	    		personas++;
+	    	}
+    	}
+    }
 }
 
