b4a6dfec3803381af9a210f5de50035779568cde
Isaac Amarilla <sc.amarilla@gmail.com>
2016-11-17 10:24:51 -0300

    Terminado apilador y estructurador

--- apilador.h
+++ apilador.h
@@ -9,64 +9,22 @@ int desencolar(struct cola * C){						//DESENCOLA EL VECTOR DE COLAS DE ESTRUCTU
     if(C->primero!=NULL){
         temporal=C->primero;
         C->primero=C->primero->sig;
-        aux=*temporal;
-        free(temporal);
 	    
     }else{
     	return NULL;
     }
-	return aux;
-
-
+	return temporal;
 }
 
 
-void insertar( struct nodo **lista, struct nodo**ultimo,  int dato ){
-	int b;
-	if(*lista==NULL){
-	 	struct nodo *nuevo;
-		nuevo= (struct nodo *)malloc( sizeof( struct nodo ) );
-		nuevo->piso=dato;
-		nuevo->sig=NULL;
-		nuevo->ant=NULL;
-		*lista=nuevo;
-		*ultimo=nuevo;
-		}
-	else {
-		struct nodo *nuevo;
-		struct nodo *actual=*lista;
-		b=contar(&(*lista), dato);
-		if(b==0){
-			nuevo= (struct nodo *)malloc( sizeof( struct nodo ) );
-			if(nuevo!=NULL){
-				nuevo->piso=dato;
-				while(actual!=NULL){
-				            
-				        if(dato<actual->piso){
-				            if(actual==*lista){
-				                *lista=nuevo;
-				            }
-				            nuevo->ant=actual->ant;
-				            nuevo->sig=actual;
-				            if(actual->ant!=NULL){
-				                actual->ant->sig=nuevo;
-				            }
-				            actual->ant=nuevo;
-				            break;
-				        }
-				        else{
-				            if(actual->sig==NULL){
-				                nuevo->ant=actual;
-				                actual->sig=nuevo;
-				                *ultimo=nuevo;
-				                break;
-				            }
-				            actual=actual->sig;
-				        }
-				}
-				
-		}
-		}
+void insertar( struct nodo * persona){
+	if(lista != NULL){
+		lista->ant=persona;
+		persona->sig=lista;
+		persona->ant=NULL;
+		lista=persona;
+	}else{
+		lista=persona;
 	}
 }
 
@@ -91,13 +49,13 @@ void eliminar(int piso){													//ELIMINA LOS NODOS QUE YA LLEGARON A DESTI
 	return 0;
 }
 
-void apilador( int piso, int direc){				//FUNCION DENTRO DEL MAIN ENCARGADO DE LLAMAR A LAS FUNCIONES AUXILIARES
+void apilador( int piso){				//FUNCION DENTRO DEL MAIN ENCARGADO DE LLAMAR A LAS FUNCIONES AUXILIARES
     int dato;
     int time;
     int eliminado=1;
     struct nodo persona;
     if(piso != -1){
-    	if(lista!= NULL && eliminado == 1){
+    	if(lista!= NULL){
     		if(subir==0){
     			time=tiempo;
     		}
@@ -108,9 +66,11 @@ void apilador( int piso, int direc){				//FUNCION DENTRO DEL MAIN ENCARGADO DE L
 	    			time=tiempo;
 	    		}
 	    	}
+    	}else{
+    		eliminado=0;
     	}
     	if(eliminado==0){
-	    	if(personas < c &&Colas[direc][piso]->primero != NULL && Colas[direc][piso]->primero->tiempo <= tiempo){
+	    	if(personas < c &&Colas[direccion][piso]->primero != NULL && Colas[direccion][piso]->primero->tiempo <= tiempo){
 	    		if(subir==0){
 	    			time=tiempo;
 	    		}
@@ -118,9 +78,12 @@ void apilador( int piso, int direc){				//FUNCION DENTRO DEL MAIN ENCARGADO DE L
 	    	}
 	    	if(subir==1 && tiempo-time >= tu){
 	    		subir=0;
-	    		persona=desencolar(Colas[direc][piso]);
-	    		insertar(persona);
-	    		personas++;
+	    		persona=desencolar(Colas[direccion][piso]);
+	    		if (persona != NULL)
+	    		{
+		    		insertar(persona);
+		    		personas++;
+	    		}
 	    	}
     	}
     }
--- simulador.h
+++ simulador.h
@@ -11,172 +11,126 @@
 
 //_______________________________________________________________________________
 
-int pisoActual=0
-int tiempo=0;
 int standBy=1;
 int direccion=1;
+int posicion=0;
 //_______________________________________________________________________________
 
 
-void simulador(int time){
-	Lista lista;
-	pila *destinos=NULL;
-	//leer solicitudes
-	//apilador(); //ver estado apilador
-  	cargar();
-	movimiento(*desapilar(&destinos),time);
-	eliminar();
-}
-// recibe lista.
-void cargar (){
-	//solicitudes
-	//pila de ascensor
-	if (direccion()){
-		while(lista.primero!=NULL){
-			if((lista.primero->piso)>=pisoActual) agregar(destinos,lista.primero->piso);
-			lista.primero=lista.primero->sig;
+void simulador()
+{
+	int piso=posicion%tp==0 ? posicion/tp : -1;
+	int b;
+	if(standBy==1){
+		for (i = 0; i < n; ++i)
+		{
+			if(haySolicitudes(n)==1){
+				standBy=0;
+			}
 		}
+		if (standBy==1)
+		{
+			if(posicion>0){
+				posicion--;
+				direccion=0;
+			}else{
+				direccion=1;
+			}
+		}
+	}
+	if(standBy==0){
+		b=0;
+		if (direccion==1)
+		{
+			for (i = posicion/tp; i < n; ++i)
+			{
+				if(haySolicitudes(i)==1){
+					b=1;
+				}
+			}
+			if(b==1){
+				mover(piso, 1)
+			}
+		}else{
+			for (i = posicion/tp; i >= 0; --i)
+			{
+				if(haySolicitudes(i)==1){
+					b=1;
+				}
+
+				if(b==1){
+					mover(piso,-1)
+				}
+			}
+		}
+		if(b==0){
+			direccion=1-direccion
+			if (direccion==1)
+			{
+				for (i = posicion/tp; i < n; ++i)
+				{
+					if(haySolicitudes(i)==1){
+						b=1;
+					}
+				}
+			}else{
+				for (i = posicion/tp; i >= 0; --i)
+				{
+					if(haySolicitudes(i)==1){
+						b=1;
+					}
+				}
+			}
+			standBy=!b;
+
+		}
+	}
+}
+
+void mover(int piso, int dir)
+{
+	int b=1;
+	if(piso==-1){
+		posicion=posicion+dir;
 	}else{
-		while(lista.ultimo!=NULL){
-			if((lista.ultimo->piso)<=pisoActual) agregar(destinos,lista.ultimo->piso);
-			lista.ultimo=lista.ultimo->ant;
+		if (seDebeEsperar(piso)==0){
+			posicion=posicion+dir;
 		}
 	}
+
 }
 
-void movimiento(int piso,int tiempo){
-
-	if (piso>pisoActual && piso <=n){
-		do
-		{
-			++pisoActual;
-			tiempoespera(tp);
+int haySolicitudes(int piso)
+{
+	int b=0;
+	for (int j = 0; j < 2; ++j)
+	{
+		if(Colas[j][i]->primero != NULL && Colas[j][i]->primero->tiempo <=tiempo){
+			b=1;
 		}
-		while (pisoActual<piso);
 	}
-	else if (piso<pisoActual && piso>=1){
-		do
-		{
-			--pisoActual;
-			tiempoespera(tp);
+	actual=lista;
+	while(actual != NULL){
+		if(actual->pdestino == piso){
+			b=1
 		}
-		while (pisoActual>piso);
+		actual=actual->sig;
 	}
-}
-// dira en que piso se encuentra..
-int posicion  ( void ){
-	
+	return b;
 }
 
-//direccion en el que va el ascensor.
-	// 1(uno) si sube y 0(cero) si baja....	
-	// acdd: acendente decendente.
-// avisa si ya esta ese piso, si esta 1, si no 0.
-int verificar( ){
-	
-}
+int seDebeEsperar(int piso){
+	int b=0;
 
-// buscara pisos por eliminar y los elimina si los hay...
-void eliminar( ){
-	
-}
-
-// realiza el tiempo de espera
-void tiempoespera(int t){
-
-}
-
-void agregar(pila **lista,int dat){
-	pila *ptrNuevo; /* apuntador a un nuevo nodo */
-	pila *ptrAnterior; /* apuntador a un nodo previo de la lista */
-	pila *ptrActual; /* apuntador al nodo actual de la lista */
-	ptrNuevo = malloc( sizeof( struct pila ) ); /* crea un nodo */
-	if ( ptrNuevo != NULL ) {
-		ptrNuevo->dato = dat; 
-		ptrNuevo->sig = NULL; 
-		ptrAnterior = NULL;
-		ptrActual = *lista;
-		while ( ptrActual != NULL && comparar(ptrActual->dato,dat)) {
-			ptrAnterior = ptrActual; 
-			ptrActual = ptrActual->sig; 
-		}
-
-		if (ptrAnterior == NULL){
-			ptrNuevo->sig = *lista;
-			*lista = ptrNuevo;
-		} else { /* inserta un nuevo nodo entre ptrAnterior y ptrActual */
-			ptrAnterior->sig = ptrNuevo;
-			ptrNuevo->sig = ptrActual;
-		}
-	} else {
-		printf("No se inserto %d. No hay memoria disponible.\n", dat );
+	if(Colas[direccion][i]->primero != NULL && Colas[direccion][i]->primero->tiempo <=tiempo){
+		b=1;
 	}
-} 
-/*void agregar(pila **lista,int x){
-    if(*lista==NULL){//Si la lista esta vacia se inicializa y se inserta
-        (*lista)=malloc(sizeof(pila));
-        (*lista)->sig=NULL;
-        (*lista)->ant=NULL;
-        (*lista)->dato=x;
-    }
-    else{//Sino se inserta por orden ascendente o descendente (dependiendo de la direccion del movimiento)
-        pila *actual;
-        pila *aux;//Nodo nuevo creado
-        aux=crearNodo(x);
-        actual=*lista;//auxiliar para buscar en la lista
-        int a;
-        while(actual!=NULL){
-        	if (actual->dato!=x){
-	            a=comparar(actual->dato,x);
-	            if(a==1){//Si el dato a insertar es [mayor (asc) o menor (des)] que el nodo actual, se inserta antes de ese nodo actual
-	                if(actual==*lista){//Si el nodo elegido es el primero
-	                  *lista=aux;//El nodo nuevo es el primero
-	                }
-	                aux->ant=actual->ant;
-	                aux->sig=actual;
-	                if(actual->ant!=NULL){//Si el nuevo no va sustituir al primero, el nodo anterior ,con respecto al nodo actual, apunta al nodo nuevo, y el nodo nuevo apunta al nodo actual
-	                    actual->ant->sig=aux;
-	                }
-	                actual->ant=aux;
-	                break;
-	            }
-	            else{//Si el nodo actual no es [menor (asc) o mayor (des)] se busca en el siguiente nodo
-	                if(actual->sig==NULL){//Si es el fin de la lista, se inserta alli
-	                    aux->ant=actual;
-	                    actual->sig=aux;
-	                    break;
-	                }
-	                actual=actual->sig;
-	            }
-
-        }
-    }
-}*/
-
-pila *crearNodo(int dat){
-    pila *aux=malloc(sizeof(pila));
-    aux->dato=dat;
-    aux->sig=NULL;
-    //aux->ant=NULL;
-    return aux;
-}
-
-int comparar(int a,int b){
-	if (direccion) return a<b;
-	return a>b;
-}
-
-int *desapilar(pila **cima){
-    static int q;
-    int *ret;
-    ret=NULL;
-    if (*cima != NULL){
-        pila *aux;
-        aux=*cima;
-        q=(aux->dato);
-        ret=&q;
-        *cima=(*cima)->sig;
-        free(aux);
-    }
-    return ret;
+	actual=lista;
+	while(actual != NULL){
+		if(actual->pdestino == piso){
+			b=1
+		}
+		actual=actual->sig;
+	}
+	return b;
 }
